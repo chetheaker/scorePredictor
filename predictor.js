4dce530689c5643fd294395de9b178e926629f53
chetheaker <che.theaker@gmail.com>
2022-01-31 14:18:11 +0100

    First minut  of goal prediction added

--- predictor.js
+++ predictor.js
@@ -1,4 +1,8 @@
-
+// Minutes in a football game (3 mins added time each half):
+const matchMinutes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+    33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, "45+1", "45+2", "45+3", 46, 47, 48, 49 , 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, "90+1", "90+2",
+    "90+3"];
 
 // Teams' home goal multipliers:
 const manCityHome = 1.25;
@@ -225,7 +229,21 @@ const scorePredictor = (home, away) => {
     } else if (awayGoalScore <= 357) {
         awayGoals = 9;
     }
-    
+    console.log(home + ": " + homeGoals);
+    console.log(away + ": " + awayGoals);
+    let totalGoals = homeGoals + awayGoals;
+    let firstGoalMinute;
+    if (totalGoals > 2) {
+        firstGoalMinute = matchMinutes[Math.floor(Math.random() * (matchMinutes.length-9))];
+        console.log("The first goal will be scored in the " + firstGoalMinute + "\' minute.");
+    } else if (totalGoals === 1) {
+        firstGoalMinute = matchMinutes[Math.floor(Math.random() * matchMinutes.length)];
+        console.log("The only goal will be scored in the " + firstGoalMinute + "\' minute.");
+    } else {
+        firstGoalMinute = matchMinutes[Math.floor(Math.random() * (matchMinutes.length))];
+        console.log("The first goal will be scored in the " + firstGoalMinute + "\' minute.");
+    }
 }
 
-scorePredictor("Manchester City", "Liverpool");
+scorePredictor("Arsenal", "Liverpool");
+
